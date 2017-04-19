dc75defbbed68d475640e6586b16aeb3b6c183b3
Jonathan Mirabile <jonathanmirabile@mail.weber.edu>
2017-04-19 10:36:39 -0600

    Initial Commit.

---
+++ Homework6/Homework6/expressionTree.cpp
@@ -0,0 +1,21 @@
+#include "expressionTree.h"
+
+ExpressionTree::ExpressionTree():BinaryTree(){
+}
+
+ExpressionTree::ExpressionTree(std::string exp):BinaryTree(){
+}
+
+void ExpressionTree::setExpression(std::string exp){
+}
+
+double ExpressionTree::getResult() const{
+   //Just to make it compile
+   return -1;
+}
+
+void ExpressionTree::printParseTreeInOrder(std::ostream& os) const{
+}
+
+void ExpressionTree::printParseTreePostOrder(std::ostream& os) const{
+}
---
+++ Homework6/Homework6/expressionTree.h
@@ -0,0 +1,21 @@
+#ifndef H_Expression_Tree
+#define H_Expression_Tree
+
+#include "binaryTree.h"
+
+#include <iostream>
+#include <string>
+
+class ExpressionTree : private BinaryTree<std::string>{
+
+   public:
+      ExpressionTree();
+      ExpressionTree(std::string exp);
+      void setExpression(std::string exp);
+      double getResult() const;
+      void printParseTreeInOrder(std::ostream& os) const;
+      void printParseTreePostOrder(std::ostream& os) const;
+};
+
+
+#endif
